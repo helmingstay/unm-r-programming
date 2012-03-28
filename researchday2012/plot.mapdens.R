3158baa2b3388bdd2383aa1eab0cc5f8c820bf86
xian <xian@ipomoea.(none)>
2012-03-27 23:14:49 -0500

    getting figure ready, added shore bird classification

--- researchday2012/plot.mapdens.R
+++ researchday2012/plot.mapdens.R
@@ -5,10 +5,10 @@ if(F){
     load('puerto_rico_shp.RData') 
 }
 
-pr_plot = ggplot() +
+pr_plot = ggplot(pr.species) +
 #Year.Collected
     coord_map() + #project = 'lagrange')
-  geom_hex( data=pr.species, aes(Decimal.Longitude, Decimal.Latitude), alpha=0.95, binwidth=c(0.1, 0.5)) +
+  geom_hex( data=pr.species, aes(Decimal.Longitude, Decimal.Latitude), alpha=0.95, binwidth=c(0.06, 0.05)) +
   labs(x='', y='') + #stat_binhex() + geom_hex() +
   ## see theme_get() for a full list of options
   opts(plot.margin=unit(c(-1,0,-1,-0.5), 'lines'), 
@@ -17,5 +17,9 @@ pr_plot = ggplot() +
     axis.text.y=theme_text(colour='black')
   ) + 
   geom_polygon( data=pr.df, aes(long,lat,group=group), 
-                fill='transparent', colour='black', width=2) 
+                fill='transparent', colour='black', width=2) +
+  facet_grid(is.shore ~ season)
+
+png('FIG-nspec.season.png', width=5000, height=5000/3, res=250)
 print(pr_plot)
+dev.off()
--- researchday2012/prep.pr.R
+++ researchday2012/prep.pr.R
@@ -7,9 +7,12 @@ pr.birds = Puerto_Rico_effort_2002_2011
 ## with() is just like a local attach -- pull out all the date info
 ## and convert to a real R-style Date
 pr.birds$Date = as.Date(with(pr.birds, {sprintf('%s-%s-%s', Year.Collected,  Month.Collected, Day.Collected)}))
+pr.birds$season = factor((pr.birds$Julian.Day %/% 90)%%4, labels=c('Winter','Spring','Summer','Fall'))
+myshoreorder = c('Anseriformes', 'Charadriiformes', 'Gruiformes', 'Ciconiiformes', 'Phoenicopteriformes', 'Podicipediformes', 'Procellariiformes', 'Podicipediformes')
+pr.birds$is.shore = pr.birds$Order %in% myshoreorder
 
 ## number of unique species per day and location
-pr.species = ddply(pr.birds, c('Decimal.Longitude', 'Decimal.Latitude', 'Date'), 
+pr.species = ddply(pr.birds, c('Decimal.Longitude', 'Decimal.Latitude', 'Date', 'season', 'is.shore'), 
     function(x) {
         ret = data.frame(nspecies=length(unique(factor(x$Scientific.Name))))
         return(ret)
